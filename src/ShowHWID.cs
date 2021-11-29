4e0c2ae73c99f7a1ffabd55789f5f9aff0d00ee8
GlebYoutuber <73891760+GlebYoutuber@users.noreply.github.com>
2021-11-29 17:15:48 +0300

    Show HWID

---
+++ src/ShowHWID.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using aLib.Microsoft;
+
+namespace ShowHWID
+{
+    public partial class ShowHWID : Form
+    {
+        public ShowHWID()
+        {
+            InitializeComponent();
+            MessageBox.Show("Here is Your HWID...", "Show Your HWID");
+            MessageBox.Show(mm_Encryptions.License.GetUHId(), "Show Your HWID");
+            Environment.Exit(0);
+        }
+    }
+}
